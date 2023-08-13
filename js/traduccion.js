d455f20fa8f5d0b3ebccbe8047f17ae255757a7c
Daniel Gutierrez <danigutierritos2817@gmail.com>
2023-08-13 06:53:06 -0500

    traduccion

---
+++ js/traduccion.js
@@ -0,0 +1,119 @@
+const translations = {
+    es: {
+      welcome: "Bienvenidos",
+      to: "A",
+       seccion1: "En WhiskerVet, velamos por el pleno bienestar de tu felino. Nuestro hospital veterinario especializado está a tu servicio las 24 horas del día, los 7 días de la semana. En nuestra institución, sostenemos firmemente la importancia de la medicina preventiva como pilar fundamental para prolongar la vida y la vitalidad de tu gato. Por ello, nos enfocamos en proporcionar asesoramiento experto y en disponer de tecnología de vanguardia, garantizando que tus adorados mininos gocen de una vida larga y saludable. Dirección:",
+        titletestimonios:"Testimonios de nuestros clientes",
+        menunosotros:"Acerca de Nosotros",
+        menuservicios:"Servicios",
+        menutestimonios:"Testimonios",
+        seccion2p1:"Exámenes exhaustivos y evaluaciones de la salud general de tu gato, incluyendo revisión de signos vitales, peso, ojos, oídos y sistema respiratorio.",
+        seccion2examenesmedicos:"Consulta Médica Integral",
+        seccion2p1:"Programa de inmunización personalizado para proteger a tu gato contra enfermedades comunes, además de estrategias de control de parásitos internos y externos.",
+        seccion2vacunacion:"Vacunación y Desparasitación",
+        seccion2p3:"Procedimientos quirúrgicos seguros para controlar la población y mejorar la salud de tu gato, reduciendo riesgos de enfermedades y comportamientos no deseados.",
+        seccion2cirugia:"Cirugía de Esterilización",
+        seccion2p4:"Mantenimiento dental especializado para prevenir enfermedades bucales y mejorar la calidad de vida de tu gato, incluyendo limpiezas y extracciones.",
+        seccion2odontologia:"Odontología Felina",
+        seccion2p5:"Atención inmediata y cuidados intensivos en situaciones de emergencia, brindando respaldo médico las 24 horas.",
+        seccion2urgencia:"Urgencias y Cuidados Intensivos",
+        seccion2p6:"Diagnóstico avanzado mediante radiografías y ecografías, que permiten detectar problemas internos y guiar tratamientos precisos.",
+        seccion2exams:"Exámenes de Imagenología",
+        seccion2p7:"Asesoramiento nutricional personalizado para garantizar que tu gato reciba la dieta adecuada para su edad, condición y necesidades específicas.",
+        seccion2nutricione:"Nutrición y Dietética Felina",
+        seccion2p8:"Evaluación de comportamiento y recomendaciones para abordar problemas conductuales, proporcionando un ambiente armonioso para tu gato y tu hogar.",
+        seccion2consulta:"Consultas de Comportamiento",
+        seccion2p9:"Cuidados enfocados en gatos de edad avanzada, incluyendo evaluación de enfermedades relacionadas con la vejez y recomendaciones para mantener su calidad de vida.",
+        seccion2atencion:"Atención Geriátrica",
+        seccion2p10:"Espacio y atención especializada para gatos que requieren hospitalización y seguimiento durante su proceso de recuperación tras cirugías o enfermedades.",
+        seccion2hospitalizacion:"Hospitalización y Recuperación",
+        testimoniotitulo:"Testimonios de nuestros clientes",
+        testimonio1:"Mi gatita Bella solía tener problemas dentales que la hacían sentir incómoda. Gracias a la odontología felina en WhiskerVet, su sonrisa ahora brilla. Las limpiezas dentales y el asesoramiento sobre su dieta han mejorado su salud en general", 
+        testimonio2:"Cuando mi gato Coco sufrió una emergencia médica, WhiskerVet estuvo allí para nosotros. Su atención rápida y cuidados intensivos salvaron la vida de Leo. Ahora, está más enérgico que nunca y le estamos eternamente agradecidos",
+        testimonio3:"Mango solía ser muy ansioso y tenía problemas de comportamiento. Después de consultar con los expertos en WhiskerVet, nos proporcionaron estrategias efectivas para calmar su ansiedad. Ahora, Mango está más tranquilo y nuestra relación es más fuerte.",
+        testimonio4:"Nuestro gato Toby envejeció y empezó a tener problemas de movilidad. Gracias a los cuidados geriátricos de WhiskerVet, ahora Toby tiene una mejor calidad de vida. Los tratamientos y recomendaciones nos han ayudado a mantenerlo activo y cómodo.",
+        testimonio5:"Mia era una gatita callejera que rescatamos, pero tenía parásitos internos y externos. WhiskerVet la desparasitó, la vacunó y nos guió en su proceso de adaptación. Ahora, Mia es parte integral de nuestra familia y está sana.",
+        testimonio6:"El equipo de WhiskerVet realizó la cirugía de esterilización de Simba con gran cuidado. La recuperación fue suave y su comportamiento mejoró notablemente. Estamos agradecidos por la atención experta que recibió Simba en todo momento.",
+        seccion3title:"¿Quiénes Somos?",
+        seccion3parrafo:"Somos una dedicada Clínica Veterinaria comprometida con el bienestar integral de tus felinos. En WhiskerVet, nuestro hospital especializado para gatos, operamos las 24 horas del día, los 7 días de la semana, para brindar atención constante y experta a tu querido compañero felino.",
+        misionparrafo:"Nuestra misión es ofrecer un cuidado excepcional y personalizado para cada gato que entre por nuestras puertas. Nos esforzamos por promover la salud y longevidad de nuestros pacientes felinos a través de servicios de calidad, tecnología avanzada y asesoramiento especializado.",
+        visionparrafo:"Aspiramos a ser la referencia en la atención veterinaria felina, destacándonos por nuestra pasión, conocimiento y enfoque en la medicina preventiva. Buscamos ser un oasis de confianza para los dueños de gatos, donde sus amigos peludos puedan disfrutar de una vida plena y enriquecedora.",
+        horarioparrafo:"En WhiskerVet, estamos comprometidos con brindar atención constante y confiable para tu felino. Nuestra clínica veterinaria para gatos está abierta las 24 horas del día, los 7 días de la semana. Ya sea que tu gato necesite atención de día o de noche, nuestro equipo estará disponible para ofrecer el cuidado necesario en cualquier momento. Tu tranquilidad y la salud de tu gato son nuestra prioridad.",
+        equipomedicoparrafo:"En WhiskerVet, contamos con un equipo médico altamente capacitado y apasionado por la salud y el bienestar de los felinos. Nuestros veterinarios especializados en gatos están comprometidos en brindar cuidados excepcionales a lo largo de las 24 horas, los 7 días de la semana. Con experiencia y dedicación, estamos aquí para ofrecer la mejor atención médica a tu compañero felino en cualquier momento que lo necesites.",
+        intalacionesparrafo:"En WhiskerVet, nos enorgullecemos de nuestras modernas instalaciones diseñadas específicamente para el confort y bienestar de los gatos. Nuestro entorno está cuidadosamente pensado para reducir el estrés y crear un ambiente tranquilo. Contamos con áreas de consulta especializadas, quirófanos equipados con tecnología avanzada, espacios de hospitalización cómodos y salas de espera relajantes. Cada rincón está diseñado pensando en los felinos, para que se sientan seguros y en casa mientras reciben la atención que merecen.",
+        contactoparrafo:"Si deseas ponerte en contacto con nosotros o visitarnos, aquí tienes la información necesaria: Dirección: Calle Felina, Nº 123, Ciudad Gatoval, Felinolandia. Teléfono: +1-555-1234 (Atendemos llamadas las 24 horas). Correo Electrónico: info@whiskervet.com. Estamos aquí para responder a tus preguntas, programar citas y brindarte toda la asistencia que necesitas para el cuidado de tu gato. ¡Esperamos verte pronto en WhiskerVet!",
+        footerurgencia:"Urgencias",
+        footertratamientos:"Tratamientos",
+        footerveterinarias:"Veterinarias",
+        footernosotros:"Nosotros",
+        footerpreguntas:"Preguntas frecuentes",
+        footerwhiskervet:"Somos una veterinaria especializada en la salud y cuidado de los felinos.",
+    },
+    en: {
+      welcome: "Welcome",
+      to: "To",
+       seccion1: "At WhiskerVet, we ensure the full well-being of your cat. Our specialized veterinary hospital is at your service 24 hours a day, 7 days a week. At our institution, we firmly support the importance of preventive medicine as a fundamental pillar to prolong the life and vitality of your cat. For this reason, we focus on providing expert advice and having cutting-edge technology, ensuring that your beloved kitties enjoy a long and healthy life. Address:",
+        titletestimonios:"Testimonials from our clients",
+        menunosotros: "About Us",
+        menuservicios: "Services",
+        menutestimonios: "Testimonials",
+        seccion2p1: "Comprehensive examinations and assessments of your cat's overall health, including vital signs, weight, eyes, ears, and respiratory system.",
+        seccion2examenesmedicos: "Comprehensive Medical Consultation",
+        seccion2p2: "Personalized immunization program to protect your cat against common diseases, along with strategies for internal and external parasite control.",
+        seccion2vacunacion: "Vaccination and Deworming",
+        seccion2p3: "Safe surgical procedures for population control and enhancing your cat's health, reducing risks of diseases and unwanted behaviors.",
+        seccion2cirugia: "Spaying and Neutering Surgery",
+        seccion2p4: "Specialized dental maintenance to prevent oral diseases and improve your cat's quality of life, including cleanings and extractions.",
+        seccion2odontologia: "Feline Dentistry",
+        seccion2p5: "Immediate care and intensive support in emergency situations, providing medical backup 24 hours a day.",
+        seccion2urgencia: "Emergencies and Intensive Care",
+        seccion2p6: "Advanced diagnostics through X-rays and ultrasounds, allowing detection of internal issues and guiding precise treatments.",
+        seccion2exams: "Imaging Examinations",
+        seccion2p7: "Personalized nutritional guidance to ensure your cat receives the proper diet for its age, condition, and specific needs.",
+        seccion2nutricione: "Feline Nutrition and Dietetics",
+        seccion2p8: "Behavioral evaluation and recommendations to address behavioral issues, providing a harmonious environment for your cat and your home.",
+        seccion2consulta: "Behavioral Consultations",
+        seccion2p9: "Focused care for senior cats, including evaluation of age-related diseases and recommendations to maintain their quality of life.",
+        seccion2atencion: "Geriatric Care",
+        seccion2p10: "Specialized space and care for cats requiring hospitalization and follow-up during recovery after surgeries or illnesses.",
+        seccion2hospitalizacion: "Hospitalization and Recovery",
+        testimoniotitulo: "Customer Testimonials",
+        testimonio1: "My kitten Bella used to have dental problems that made her uncomfortable. Thanks to feline dentistry at WhiskerVet, her smile now shines. Dental cleanings and dietary advice have improved her overall health.",
+        testimonio2: "When my cat Coco had a medical emergency, WhiskerVet was there for us. Their prompt attention and intensive care saved Leo's life. Now, he's more energetic than ever, and we're eternally grateful.",
+        testimonio3: "Mango used to be very anxious and had behavioral issues. After consulting with the experts at WhiskerVet, they provided effective strategies to calm his anxiety. Now, Mango is calmer, and our bond is stronger.",
+        testimonio4: "Our cat Toby aged and started experiencing mobility issues. Thanks to WhiskerVet's geriatric care, Toby now has a better quality of life. The treatments and recommendations have helped us keep him active and comfortable.",
+        testimonio5: "Mia was a stray kitten we rescued, but she had internal and external parasites. WhiskerVet dewormed her, vaccinated her, and guided us through her adaptation process. Now, Mia is an integral part of our family and is healthy.",
+        testimonio6: "The WhiskerVet team performed Simba's sterilization surgery with great care. The recovery was smooth, and his behavior improved significantly. We're grateful for the expert attention Simba received throughout the process.",
+        seccion3title: "Who We Are",
+        seccion3parrafo: "We are a dedicated Veterinary Clinic committed to the holistic well-being of your feline friends. At WhiskerVet, our specialized cat hospital operates 24/7, providing constant and expert care for your beloved feline companion.",
+        misionparrafo: "Our mission is to offer exceptional and personalized care for every cat that enters our doors. We strive to promote the health and longevity of our feline patients through quality services, advanced technology, and specialized advice.",
+        visionparrafo: "We aspire to be a reference in feline veterinary care, standing out for our passion, knowledge, and focus on preventive medicine. We aim to be a trusted oasis for cat owners, where their furry friends can enjoy a full and enriching life.",
+        horarioparrafo: "At WhiskerVet, we are committed to providing constant and reliable care for your feline friend. Our cat veterinary clinic is open 24/7. Whether your cat needs care during the day or night, our team is available to provide the necessary attention at any time. Your peace of mind and your cat's health are our priority.",
+        equipomedicoparrafo: "At WhiskerVet, we take pride in our highly trained and passionate medical team dedicated to the health and well-being of felines. Our cat-specialized veterinarians are committed to providing exceptional care 24/7. With experience and dedication, we are here to provide the best medical attention to your feline companion whenever you need it.",
+        intalacionesparrafo: "At WhiskerVet, we are proud of our modern facilities designed specifically for the comfort and well-being of cats. Our environment is carefully designed to reduce stress and create a tranquil atmosphere. We have specialized consultation areas, operating rooms equipped with advanced technology, comfortable hospitalization spaces, and relaxing waiting rooms. Every corner is designed with cats in mind, making them feel secure and at home while receiving the care they deserve.",
+        contactoparrafo: "If you want to get in touch with us or visit us, here's the necessary information: Address: Felina Street, No. 123, Gatoval City, Felinoland. Phone: +1-555-1234 (We answer calls 24/7). Email: info@whiskervet.com. We are here to answer your questions, schedule appointments, and provide you with all the assistance you need for your cat's care. We look forward to seeing you soon at WhiskerVet!",
+        footerurgencia: "Emergencies",
+        footertratamientos: "Treatments",
+        footerveterinarias: "Veterinaries",
+        footernosotros: "About Us",
+        footerpreguntas: "Frequently Asked Questions",
+        footerwhiskervet: "We are a specialized clinic for the health and care of felines."        
+    }
+  };
+
+  function translatePage(language) {
+    const elementsToTranslate = document.querySelectorAll("[data-translate]");
+    elementsToTranslate.forEach(element => {
+      const key = element.getAttribute("data-translate");
+      element.textContent = translations[language][key];
+    });
+  }
+
+  const languageSelect = document.getElementById("language");
+  languageSelect.addEventListener("change", event => {
+    const selectedLanguage = event.target.value;
+    translatePage(selectedLanguage);
+  });
+
+  // Initial translation
+  translatePage(languageSelect.value);
