02cb45fa066b93c81e606cc6ced6a0d77d3de19f
Arduousbook <davids_barretom@javeriana.edu.co>
2023-08-13 12:41:41 -0500

    Subida de página con estilo parallax.

---
+++ paginaParallax/js/app.js
@@ -0,0 +1,27 @@
+document.querySelector('.arrow-link').addEventListener('click', function (e) {
+    // Prevenir el comportamiento predeterminado del enlace
+    e.preventDefault();
+
+    // Obtiene el valor del atributo "href" del enlace
+    const target = this.getAttribute('href');
+
+    // Obtiene la posición (offsetTop) de la sección objetivo
+    const targetPosition = document.querySelector(target).offsetTop;
+
+    // Duración del desplazamiento en milisegundos
+    const duration = 10000000; // Puedes ajustar esta duración según tu preferencia
+
+    // Realiza el desplazamiento suave utilizando animaciones
+    window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth' // Crea un efecto de desplazamiento suave
+    });
+});
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
